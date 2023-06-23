bbb0fb135b5bfe60bd076a962b1dc0cde9a537f6
Thainá Freitas <thaina.augustafs@outlook.com>
2023-06-23 14:47:05 -0300

    Aula 4

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,7 +1,6 @@
 import { getUser } from '@/lib/auth'
 import Image from 'next/image'
 
-
 export function Profile() {
     const { name, avatarUrl } = getUser()
 
@@ -19,7 +18,12 @@ export function Profile() {
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
         
-        <a href='' className='block text-red-400 hover:text-red-300'>Quero sair</a>
+        <a 
+        href='/api/auth/logout' 
+        className='block text-red-400 hover:text-red-300'
+        >
+          Quero sair
+        </a>
         </p>
     </div>
   )
