94097445a5fae47ae18bc40852f8742ffc706009
Thainá Freitas <thaina.augustafs@outlook.com>
2023-06-26 23:25:18 -0300

    Aula 5

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     
     
       {/* Right */}
-      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+      <div className="flex max-h-screen overflow-y-scroll flex-col bg-[url(../assets/bg-stars.svg)] bg-cover">
       {children}
     </div>
   </main>
